15ed3c2d7f8c4dfd6e342d3cd626dc772b2475e0
endo_cadelanne <cramitlaferriere@gmail.com>
2026-08-20 16:56:25 -0700

    draws a triangle finally

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import pygame
 
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+from constants import *
 from logger import log_state
+from player import *
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.init() #Initialize the pygame module
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # set screen size
+    clock = pygame.time.Clock() # create clock object
+    dt: float = 0.0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
